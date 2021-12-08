82723f4616accdf8a23a357949f23406e2d8b82b
uphillbattle <uphillbattle@users.noreply.github.com>
2021-12-08 15:58:26 +0100

    Variable price sensor now includes hourly rates for "today" and "tomorrow" - same style as the nordpool sensor. Sensors also include price level (as reported by the Elvia API).

--- nettleie_elvia.py
+++ nettleie_elvia.py
@@ -35,13 +35,19 @@ class NettleieElvia(hass.Hass):
   def set_times(self):
     self.current_datetime   = datetime.datetime.now()
     self.next_hour_datetime = self.current_datetime + datetime.timedelta(hours=1)
+    self.next_day_datetime  = self.current_datetime + datetime.timedelta(hours=48)
     self.pretty_last_hour   = str(self.current_datetime.year) + "-" + str(self.current_datetime.month).zfill(2) + "-" + \
-                              str(self.current_datetime.day).zfill(2) + "T" + str(self.current_datetime.hour).zfill(2) + ":00:00"
-    self.pretty_next_hour   = str(self.next_hour_datetime.year) + "-" + str(self.next_hour_datetime.month).zfill(2) + "-" + \
-                              str(self.next_hour_datetime.day).zfill(2) + "T" + str(self.next_hour_datetime.hour).zfill(2) + ":00:00"
+                              str(self.current_datetime.day).zfill(2) + "T" + "00:00:00"
+    self.pretty_next_hour   = str(self.next_day_datetime.year) + "-" + str(self.next_day_datetime.month).zfill(2) + "-" + \
+                              str(self.next_day_datetime.day).zfill(2) + "T" + "00:00:00"
 
     self.body["startTime"]  = self.pretty_last_hour
     self.body["endTime"]    = self.pretty_next_hour
+
+    self.current_hour       = self.current_datetime.hour
+
+    self.todayString = str(self.current_datetime.year) + "-" + str(self.current_datetime.month).zfill(2) + "-" + \
+                       str(self.current_datetime.day).zfill(2)
 
 
   def fetch_data(self, retry_function, wait_period):
@@ -51,12 +57,48 @@ class NettleieElvia(hass.Hass):
       self.log('__function__: Ooops, API request failed, retrying in {} seconds...\n{}'.format(wait_period, e), log="main_log", level="WARNING")
       self.run_in(retry_function, wait_period)
 
+
   def set_states(self):
-    self.maler_response          = json.loads(self.maler_response_json.text)
-    self.fixed_price_per_hour    = self.maler_response["gridTariffCollections"][0]["gridTariff"]["tariffPrice"]["priceInfo"][0]["fixedPrices"][0]["priceLevel"][0]["total"]
-    self.variable_price_per_hour = self.maler_response["gridTariffCollections"][0]["gridTariff"]["tariffPrice"]["priceInfo"][0]["variablePrice"]["total"]
-    self.set_state(self.args["sensorname"] + '_kapasitetsledd', state=self.fixed_price_per_hour,    attributes={'friendly_name': self.args["sensoralias"] + ' per time', 'unit_of_measurement': 'NOK/h', 'icon': 'mdi:currency-usd'})
-    self.set_state(self.args["sensorname"] + '_forbruksledd',   state=self.variable_price_per_hour, attributes={'friendly_name': self.args["sensoralias"] + ' per kWh',  'unit_of_measurement': 'NOK/kWh', 'icon': 'mdi:currency-usd'})
+    self.maler_response = json.loads(self.maler_response_json.text)
+    self.priceInfo      = self.maler_response["gridTariffCollections"][0]["gridTariff"]["tariffPrice"]["priceInfo"]
+
+    self.variable_price_per_hour_array_today_raw    = []
+    self.variable_price_per_hour_array_tomorrow_raw = []
+    self.variable_price_per_hour_array_today        = []
+    self.variable_price_per_hour_array_tomorrow     = []
+    for element in self.priceInfo:
+      startTime = element["startTime"]
+      endTime   = element["expiredAt"]
+      value     = element["variablePrice"]["total"]
+      if startTime[0:10] == self.todayString:
+        self.variable_price_per_hour_array_today_raw.append({"start": startTime, "end": endTime, "value": value})
+        self.variable_price_per_hour_array_today.append(value)
+      else:
+        self.variable_price_per_hour_array_tomorrow_raw.append({"start": startTime, "end": endTime, "value": value})
+        self.variable_price_per_hour_array_tomorrow.append(value)
+
+    self.fixed_price_per_hour    = self.priceInfo[self.current_hour]["fixedPrices"][0]["priceLevel"][0]["total"]
+    self.fixed_price_level       = self.priceInfo[self.current_hour]["fixedPrices"][0]["priceLevel"][0]["level"]
+    self.variable_price_per_hour = self.priceInfo[self.current_hour]["variablePrice"]["total"]
+    self.variable_price_level    = self.priceInfo[self.current_hour]["variablePrice"]["level"]
+
+    self.set_state(self.args["sensorname"] + '_kapasitetsledd', \
+                   state=self.fixed_price_per_hour, \
+                   attributes={'friendly_name': self.args["sensoralias"] + ' per time', \
+                               'unit_of_measurement': 'NOK/h', \
+                               'icon': 'mdi:currency-usd', \
+                               'level': self.fixed_price_level})
+
+    self.set_state(self.args["sensorname"] + '_forbruksledd', \
+                   state=self.variable_price_per_hour, \
+                   attributes={'friendly_name': self.args["sensoralias"] + ' per kWh', \
+                               'unit_of_measurement': 'NOK/kWh', \
+                               'icon': 'mdi:currency-usd', \
+                               'level': self.variable_price_level,
+                               'today': self.variable_price_per_hour_array_today, \
+                               'tomorrow': self.variable_price_per_hour_array_tomorrow, \
+                               'raw_today': self.variable_price_per_hour_array_today_raw, \
+                               'raw_tomorrow': self.variable_price_per_hour_array_tomorrow_raw})
 
 
   def output_log(self):
