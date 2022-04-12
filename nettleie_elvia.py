92244bd2267adfd0bcc1276f5d3d4a973b06491c
uphillbattle <uphillbattle@users.noreply.github.com>
2022-04-12 09:50:18 +0200

    Updated to conform with new DIGIN API

--- nettleie_elvia.py
+++ nettleie_elvia.py
@@ -2,6 +2,7 @@ import appdaemon.plugins.hass.hassapi as hass
 import requests
 import json
 import datetime
+import time
 
 
 
@@ -16,7 +17,7 @@ class NettleieElvia(hass.Hass):
 
   def hourly_call(self, kwargs):
     self.set_times()
-    self.fetch_data(self.hourly_call, 60)
+    self.fetch_data(self.hourly_call, 120)
     self.set_correction()
     self.set_states()
 
@@ -30,7 +31,7 @@ class NettleieElvia(hass.Hass):
   def set_request_data(self):
     self.headers   = {"Ocp-Apim-Subscription-Key": self.args["ocp_apim_subscription_key"],
                       "Content-Type": "application/json"}
-    self.url       = "https://elvia.azure-api.net/grid-tariff/api/1/tariffquery/meteringpointsgridtariffs"
+    self.url       = "https://elvia.azure-api.net/grid-tariff/digin/api/1.0/tariffquery/meteringpointsgridtariffs"
     self.body      = {"meteringPointIds": [ self.args["malerid"] ]}
 
   def set_correction_data(self):
@@ -38,14 +39,19 @@ class NettleieElvia(hass.Hass):
     self.correction_amount     = (0.1669 - 0.0891)*1.25
 
   def set_times(self):
+    localtime = time.localtime()
+    if localtime.tm_isdst > 0:
+        zoneadjust = "+02:00"
+    else:
+        zoneadjust = "+01:00"
     self.current_datetime   = datetime.datetime.now()
     self.next_hour_datetime = self.current_datetime + datetime.timedelta(hours=1)
     self.tomorrow_datetime  = self.current_datetime + datetime.timedelta(hours=24)
     self.next_day_datetime  = self.current_datetime + datetime.timedelta(hours=48)
     self.pretty_last_hour   = str(self.current_datetime.year) + "-" + str(self.current_datetime.month).zfill(2) + "-" + \
-                              str(self.current_datetime.day).zfill(2) + "T" + "00:00:00"
+                              str(self.current_datetime.day).zfill(2) + "T" + "00:00:00" + zoneadjust
     self.pretty_next_hour   = str(self.next_day_datetime.year) + "-" + str(self.next_day_datetime.month).zfill(2) + "-" + \
-                              str(self.next_day_datetime.day).zfill(2) + "T" + "00:00:00"
+                              str(self.next_day_datetime.day).zfill(2) + "T" + "00:00:00" + zoneadjust
 
     self.body["startTime"]  = self.pretty_last_hour
     self.body["endTime"]    = self.pretty_next_hour
@@ -77,16 +83,16 @@ class NettleieElvia(hass.Hass):
 
   def set_states(self):
     self.maler_response = json.loads(self.maler_response_json.text)
-    self.priceInfo      = self.maler_response["gridTariffCollections"][0]["gridTariff"]["tariffPrice"]["priceInfo"]
+    self.priceInfo      = self.maler_response["gridTariffCollections"][0]["gridTariff"]["tariffPrice"]
 
     self.variable_price_per_hour_array_today_raw    = []
     self.variable_price_per_hour_array_tomorrow_raw = []
     self.variable_price_per_hour_array_today        = []
     self.variable_price_per_hour_array_tomorrow     = []
-    for element in self.priceInfo:
+    for element in self.priceInfo["hours"]:
       startTime = element["startTime"]
       endTime   = element["expiredAt"]
-      value     = element["variablePrice"]["total"]
+      value     = element["energyPrice"]["total"]
       if startTime[0:10] == self.todayString:
         self.variable_price_per_hour_array_today_raw.append({"start": startTime, "end": endTime, "value": value - self.correction_today})
         self.variable_price_per_hour_array_today.append(value - self.correction_today)
@@ -94,24 +100,20 @@ class NettleieElvia(hass.Hass):
         self.variable_price_per_hour_array_tomorrow_raw.append({"start": startTime, "end": endTime, "value": value - self.correction_tomorrow})
         self.variable_price_per_hour_array_tomorrow.append(value - self.correction_tomorrow)
 
-    self.fixed_price_per_hour    = self.priceInfo[self.current_hour]["fixedPrices"][0]["priceLevel"][0]["total"]
-    self.fixed_price_level       = self.priceInfo[self.current_hour]["fixedPrices"][0]["priceLevel"][0]["level"]
-    self.variable_price_per_hour = self.priceInfo[self.current_hour]["variablePrice"]["total"] - self.correction_today
-    self.variable_price_level    = self.priceInfo[self.current_hour]["variablePrice"]["level"]
+    self.fixed_price_per_hour    = self.priceInfo["priceInfo"]["fixedPrices"][0]["priceLevels"][0]["hourPrices"][0]["total"]
+    self.variable_price_per_hour = self.priceInfo["priceInfo"]["energyPrices"][0]["total"] - self.correction_today
 
     self.set_state(self.args["sensorname"] + '_kapasitetsledd', \
                    state=self.fixed_price_per_hour, \
                    attributes={'friendly_name': self.args["sensoralias"] + ' per time', \
                                'unit_of_measurement': 'NOK/h', \
-                               'icon': 'mdi:currency-usd', \
-                               'level': self.fixed_price_level})
+                               'icon': 'mdi:currency-usd'})
 
     self.set_state(self.args["sensorname"] + '_forbruksledd', \
                    state=self.variable_price_per_hour, \
                    attributes={'friendly_name': self.args["sensoralias"] + ' per kWh', \
                                'unit_of_measurement': 'NOK/kWh', \
-                               'icon': 'mdi:currency-usd', \
-                               'level': self.variable_price_level,
+                               'icon': 'mdi:currency-usd',
                                'today': self.variable_price_per_hour_array_today, \
                                'tomorrow': self.variable_price_per_hour_array_tomorrow, \
                                'raw_today': self.variable_price_per_hour_array_today_raw, \
